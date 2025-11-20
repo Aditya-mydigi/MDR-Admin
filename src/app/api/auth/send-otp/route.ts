8f38aca1f1d05771de2e1e25c759a118ee81bc8f
Aditya-mydigi <aditya@mydigirecords.in>
2025-11-20 09:22:40 +0530

    change the emails for the main auth members only..

--- src/app/api/auth/send-otp/route.ts
+++ src/app/api/auth/send-otp/route.ts
@@ -2,13 +2,9 @@ import { NextResponse } from "next/server";
 import { generateAndSendOtp } from "@/lib/email";
 
 export const authorizedEmails = [
-  "support@mydigirecords.com",
   "aditya.amparmar@gmail.com",
-  "chiragbora2@gmail.com",
   "akash@mydigirecords.com",
-  "rahul@mydigirecords.com",
   "hemant@mydigirecords.com",
-  "saroj@mydigirecords.com",
 ];
 
 export async function POST(req: Request) {
